efe5416d8629c3ace66c9815bd4a14b9c3bb2b53
Rohan Vinay Chaudhary <80825254+RohanRVC@users.noreply.github.com>
2024-03-25 12:15:33 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -9,7 +9,7 @@ known_abbreviations = {
     "uae": "United Arab Emirates",
     "prc": "People's Republic of China", 
     "russia": "Russian Federation",  # Common name, full name is "Russian Federation"
-    "sa": "Saudi Arabia",
+    "sa": "Saudi Arabia", 
     "rsa": "Republic of South Africa",
     "drc": "Democratic Republic of the Congo",
     "roc": "Republic of China",  # Refers to Taiwan 
