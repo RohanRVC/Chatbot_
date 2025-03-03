c2197dabffd84ec45eeed822d627785c67d45356
Rohan Vinay Chaudhary <80825254+RohanRVC@users.noreply.github.com>
2025-03-03 12:42:07 +0530

    Update qa_chat.py

--- qa_chat.py
+++ qa_chat.py
@@ -9,7 +9,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
        
 # now wee are writing function to load Gemini Pro mode land get response           
 model=genai.GenerativeModel("gemini-pro")      
-chat=model.start_chat(history=[])  
+chat=model.start_chat(history=[])   
     
 def get_gemini_response(question):   
     response=chat.send_message(question,stream=True) # as llm modelis giving u the output we will steam and show the output  
